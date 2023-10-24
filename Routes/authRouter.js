da1a54052510bbe2af167033791f90cb047bcbc2
IT20168704 <it20168704@my.sliit.lk>
2023-10-24 13:42:58 +0530

    Add a endpoint for forgot password

--- Routes/authRouter.js
+++ Routes/authRouter.js
@@ -5,6 +5,8 @@ const router = express.Router();
 
 router.route('/signup').post(authController.signup);
 router.route('/login').post(authController.login);
+router.route('/forgotPassword').post(authController.forgotPassword);
+router.route('/resetPassword/:token').patch(authController.resetPassword);
 
 
 router.route('/all').get(authController.protect, authController.getAllUsers);
