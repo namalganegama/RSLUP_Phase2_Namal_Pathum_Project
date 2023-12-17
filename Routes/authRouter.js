fe2674d51ca34b14dcdf59e0ddbb5c350d179875
Namal Pathum Wishvajith <it20168704@my.sliit.lk>
2023-12-17 07:46:52 +0530

    add user roles

--- Routes/authRouter.js
+++ Routes/authRouter.js
@@ -3,12 +3,12 @@ const authController = require('./../Controllers/authController')
 
 const router = express.Router();
 
-router.route('/signup').post(authController.signup);
+router.route('/signup').post(authController.signup, authController.restrict('administrator, ticket agent,') );
 router.route('/login').post(authController.login);
 router.route('/forgotPassword').post(authController.forgotPassword);
 router.route('/resetPassword/:token').patch(authController.resetPassword);
 
-router.route('/all').get(authController.protect, authController.getAllUsers);
+router.route('/all').get(authController.protect,authController.restrict('administrator, ticket agent,'),  authController.getAllUsers);
 
 router.route('/:userId')
 .get(authController.protect, authController.getUser)
@@ -16,7 +16,7 @@ router.route('/:userId')
 
 .delete(
     authController.protect, 
-    authController.restrict('admin', 'developer'), 
+    authController.restrict('administrator, ticket agent,'), 
     authController.deleteUser
     )
 
