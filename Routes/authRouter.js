9e7e1c1455c93ef6ea4c2f0f575eec081ab812fe
Namal Pathum Wishvajith <it20168704@my.sliit.lk>
2023-12-17 07:51:24 +0530

    Apply Best Practices

--- Routes/authRouter.js
+++ Routes/authRouter.js
@@ -3,16 +3,31 @@ const authController = require('./../Controllers/authController')
 
 const router = express.Router();
 
-router.route('/signup').post(authController.signup, authController.restrict('administrator, ticket agent,') );
+router.route('/signup').post(
+    authController.signup, 
+    authController.restrict('administrator, ticket agent,') 
+    );
 router.route('/login').post(authController.login);
+
 router.route('/forgotPassword').post(authController.forgotPassword);
+
 router.route('/resetPassword/:token').patch(authController.resetPassword);
 
-router.route('/all').get(authController.protect,authController.restrict('administrator, ticket agent,'),  authController.getAllUsers);
+router.route('/all').get(
+    authController.protect,
+    authController.restrict('administrator, ticket agent,'),  
+    authController.getAllUsers
+    );
 
 router.route('/:userId')
-.get(authController.protect, authController.getUser)
-.put(authController.protect, authController.updateUser)
+.get(
+    authController.protect, 
+    authController.getUser
+    )
+.put(
+    authController.protect, 
+    authController.updateUser
+    )
 
 .delete(
     authController.protect, 
